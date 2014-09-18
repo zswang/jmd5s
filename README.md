a170003057a571188aa1aa1154a72bd281042441
Zswang <wjhu111@21cn.com>
2014-09-18 13:48:48 +0800

    添加代码结构

---
+++ README.md
@@ -0,0 +1,28 @@
+jmd5s(*5)
+======
+
+[![Build Status](https://img.shields.io/travis/zswang/jmd5s/master.svg)](https://travis-ci.org/zswang/jmd5s)
+[![NPM version](https://img.shields.io/npm/v/jmd5s.svg)](http://badge.fury.io/js/jmd5s)
+
+## 概述
+
+jmd5s 是 md5 函数的封装，主要是为方便用 bower 管理
+
+## API
+
+```javascript
+/**
+* 进行 md5 编码
+* @param{String} str 需要编码的字符串
+* @return{String} 返回对应的 MD5 十六进制字符串
+*/
+function encode(str) { }
+```
+## 示例
+
+```javascript
+var password = prompt("input password"); // foo
+if (jmd5s.encode(password) === 'acbd18db4cc2f85cedef654fccc4a4d8') {
+  alert('Login successful.');
+}
+```
---
+++ test/test.js
@@ -0,0 +1,34 @@
+var assert = require('should');
+var jmd5s = require('..');
+var fs = require('fs');
+var util = require('util');
+
+var crypto = require('crypto');
+
+function md5encode(text) {
+  var md5 = crypto.createHash('md5');
+  md5.update(text, 'utf-8');
+  return md5.digest('hex');
+}
+
+describe('encode-asscii', function() {
+  for (var i = 0; i < 10; i++) {
+    var text = 'index' + i;
+    it(JSON.stringify(text), (function(text) {
+      return function() {
+        assert.equal(md5encode(text), jmd5s.encode(text));
+      }
+    })(text));
+  }
+});
+
+describe('encode-utf8', function() {
+  for (var i = 0; i < 10; i++) {
+    var text = '汉字' + i;
+    it(JSON.stringify(text), (function(text) {
+      return function() {
+        assert.equal(md5encode(text), jmd5s.encode(text));
+      }
+    })(text));
+  }
+});
